9026534927285f8394da87a6d359494dcf3f0556
Anvi Bhople <146471828+AnviBhople@users.noreply.github.com>
2025-02-08 23:28:04 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,22 @@
+function showFlowers() {
+	document.getElementById("garden").style.bottom = "20rem";
+	createHearts();
+
+	setTimeout(() => {
+		document.getElementById("valentineBtn").innerHTML =
+			"Will you be my Valentine? ❤️";
+	}, 1500);
+}
+
+function createHearts() {
+	for (let i = 0; i <= 10; i++) {
+		let heart = document.createElement("div");
+		heart.innerHTML = "❤️❤️  ❤️❤️";
+		heart.classList.add("heart");
+		heart.style.left = Math.random() * window.innerWidth + "px";
+		heart.style.animationDuration = Math.random() * 2 + 2 + "s";
+		document.body.appendChild(heart);
+	}
+}
+
+setTimeout(showFlowers, 1000);
